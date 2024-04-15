f66c183bbde547bc5110fb1bbf5f647be93af9d1
leeggon <138864974+leeggon@users.noreply.github.com>
2024-04-15 23:56:04 +0900

    docs: BOJ 가장 긴 증가하는 부분 수열 풀이

---
+++ BOJ/G2/가장_긴_증가하는_부분_수열_2/BOJ12015.java
@@ -0,0 +1,14 @@
+package BOJ.G2.가장_긴_증가하는_부분_수열_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ12015 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        
+    }
+}
--- BOJ/G2/색종이_붙이기.java
+++
@@ -1,4 +0,0 @@
-package BOJ.G2;
-
-public class 색종이_붙이기 {
-}
