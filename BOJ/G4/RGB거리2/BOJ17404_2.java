507e1eeb897929bb6c9061b98817fed458f06d2a
leeggon <138864974+leeggon@users.noreply.github.com>
2024-05-02 06:55:17 +0900

    docs:BOJ 연구소3 풀이

---
+++ BOJ/G4/RGB거리2/BOJ17404_2.java
@@ -0,0 +1,16 @@
+package BOJ.G4.RGB거리2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ17404_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N + 1][3];
+    }
+}
