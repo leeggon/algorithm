8cabf98b9f4b6bbcab6d886de4586f36b2d7d4d7
leeggon <138864974+leeggon@users.noreply.github.com>
2024-06-16 23:40:03 +0900

    Docs : Programmers 뒤에 있는 큰수 찾기 풀이

---
+++ Programmers/Lv2/뒤에있는_큰수_찾기/뒤에있는_큰수_찾기.java
@@ -0,0 +1,30 @@
+package Programmers.Lv2.뒤에있는_큰수_찾기;
+
+import java.util.*;
+
+class 뒤에있는_큰수_찾기 {
+    public int[] solution(int[] numbers) {
+        int[] result = new int[numbers.length];
+        Arrays.fill(result, -1);
+
+        Stack<int[]> stack = new Stack<>();
+        stack.push(new int[]{0, numbers[0]});
+        for (int i=1; i<numbers.length; i++) {
+            int curNum = numbers[i];
+
+            while (!stack.isEmpty()) {
+                if (stack.peek()[1] < curNum) {
+                    int[] peek = stack.pop();
+                    result[peek[0]] = curNum;
+                } else {
+                    break;
+                }
+            }
+
+            stack.push(new int[]{i, curNum});
+        }
+
+
+        return result;
+    }
+}
