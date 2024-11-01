f11126bcadb5afe8ce3a95097409ea032e4f7677
이경곤 <leeggon1213@naver.com>
2024-11-01 23:15:38 +0900

    feat: PROGRAMMERS 줄 서는 방법 풀이

---
+++ programmers/level2/줄서는방법/줄서는방법.java
@@ -0,0 +1,34 @@
+package programmers.level2.줄서는방법;
+
+import java.util.*;
+
+class 줄서는방법 {
+	public int[] solution(int n, long k) {
+		List<Integer> numList = new ArrayList<>();
+		for (int i=1; i<=n; i++) {
+			numList.add(i);
+		}
+
+		long dividor = 1;
+		for (int i=1; i<n; i++) {
+			dividor = dividor * i;
+		}
+
+		int[] result = new int[n];
+		k--;
+		for (int i=n; i>=1; i--) {
+			long q = k / dividor;
+			long r = k % dividor;
+			result[n - i] = numList.get((int) q);
+			numList.remove((int) q);
+
+			k = r;
+
+			if (i > 1) {
+				dividor /= (i - 1);
+			}
+		}
+
+		return result;
+	}
+}
